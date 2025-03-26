630e4abf20bb842d26f273d0a47e943820abb475
djaaktkfkdgody <165289837+djaaktkfkdgody@users.noreply.github.com>
2025-03-26 10:42:46 +0900

    파일 추가

---
+++ d.py
@@ -0,0 +1,10 @@
+# a = int(input("a : "))
+# b = int(input("b : "))
+
+# result = a % b
+# print(result)
+
+a = input("a : ")
+b = input("b : ")
+
+print(int(a % b))
---
+++ index.py
@@ -0,0 +1,41 @@
+# end option
+# print("Hello python", end=" ")
+
+# sep option
+# print("hello", "python", sep="")
+
+# singer = input("who is favorite singer?")
+# print("favorite singer is", singer)
+
+# a = 10
+# a = 5
+# print(a)
+
+# a, b, c = 1, 1.1, "a"
+# print(a, b, c)
+
+
+# print(100-2/7+9*3)
+# print((100-2)/7+(9*3))
+
+# 복합대입연산자
+# num = 10
+# num += 20
+# print(num)
+
+# num2 = 10
+# num2 -= 20
+# print(num2)
+
+# num3 = 10
+# num3
+
+
+# a, b = map(int, input().strip().split(' '))
+# # strip 사이이공백제거, split 구분자
+# print(a, b)
+
+
+출력 = "{{{0:^9}}}".format("중괄호")
+text = f'문자열 실습입니다. {출력}를 출력해보세요'
+print(text)
