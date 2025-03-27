c2d9f364e5dcf90976c2bb6210c616ded40c3134
djaaktkfkdgody <kus09006@daum.net>
2025-03-27 10:34:39 +0900

    Create 0327.py

---
+++ 파이썬/0327.py
@@ -0,0 +1,83 @@
+# season = ["봄", "여름", "가을", "겨울"]
+# print(season)
+# print('season[0]', season[0])
+# print('season[1]', season[1])
+
+# text = 'hello'
+# letters = list(text)
+# print('letters', letters)
+
+# shop = ['반팔', '청바지', '이어폰', ['무선 키보드', '유선 키보드', '기계식 키보드']]
+# print(shop)
+# print(shop[0:])
+# print(shop[:-1])
+# print(shop[:-2])
+
+# shop[0] = "긴팔"
+# print(shop)
+
+# del shop[0]
+# print(shop)
+
+# del shop[0:1]
+# print(shop)
+
+# a = [1, 2, 3]
+# b = [4, 5]
+
+# print(a + b)
+# print(a[0] + b[0])
+# # print(a[0:3] + b[0:2])
+
+# num1 = [5, 2, 7, 3]
+# print(num1)
+# text = 'hello, world'
+# list1 = list(text)
+# print(list1)
+# num_asc = sorted(num1)
+# print(num_asc)
+# list1_asc = sorted(list1)
+# print('list1_asc', list1_asc)
+# num_desc = sorted(num1, reverse=True)
+# print('num_desc', num_desc)
+
+# print('num1', num1)
+# num1.sort()
+# print('num1', num1)
+# # num1.sort(reverse=True)
+# num1.reverse()
+# print('num1', num1)
+
+# text2 = 'hello, world hello, world ahello, world hello, world'
+# list2 = (list(text2))
+# print(list2.index('a'))
+
+# num2 = [1, 2, 3, 4, 5, 6]
+# print(num2)
+# num2.append(7)
+# print(num2)
+# num2.pop()
+# print(num2)
+# num2.pop(2)
+# print(num2)
+# num2.remove(4)
+# print(num2)
+# num2.insert(2, 'a')
+# print(num2)
+# num2.clear()
+# print(num2)
+# print(num1.count('t'))
+# print(num1.count(3))
+
+rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple']
+print(rainbow[2])
+print(sorted(rainbow))
+# rainbow.sort()
+print(rainbow)
+
+rainbow.append('white')
+# rainbow.insert(7, 'white')
+print(rainbow)
+
+del rainbow[2:6]
+print(rainbow)
